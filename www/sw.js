046ff7e12037fa6502efddc921f0efc917e05900
mandeyatayt-ux <mandeyatayt-ux@users.noreply.github.com>
2026-09-20 20:03:45 +0000

    Mobile fit: restrict body touch panning to vertical on iOS (horizontal scrollers opt back in); profile hero stats wrap on phones; SW pv-v57

--- www/sw.js
+++ www/sw.js
@@ -1,5 +1,5 @@
 // Padel Villains Service Worker
-const CACHE_VERSION = 'pv-v56';
+const CACHE_VERSION = 'pv-v57';
 const CACHE_NAME = `padel-villains-${CACHE_VERSION}`;
 
 // Core assets to cache for offline use
